88f7e8bd7df7cc9f2fba2959ecec84eac9fea2ed
Jodoktus <jodoktus@proton.me>
2025-07-20 12:57:46 +0200

    turn a .gb file into binary
    open file read code see what a byte is and print the bits

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,34 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("gameboyzig_lib");
+const U3 = std.meta.Int(.unsigned, 3);
+
+const U1 = std.meta.Int(.unsigned, 1);
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    const file = try std.fs.cwd().openFile("tetris.gb", .{});
+    defer file.close();
+
+    const file_size = try file.getEndPos();
+    const buffer = try allocator.alloc(u8, file_size);
+    defer allocator.free(buffer);
+    var list = std.ArrayList([]U1).init(allocator);
+
+    _ = try file.readAll(buffer);
+
+    const stdout = std.io.getStdOut().writer();
+    const mask: u8 = 0b1000_0000;
+    for (buffer) |byte| {
+        var bits: [8]U1 = undefined;
+
+        for (0..8) |i| {
+            const u8i: U3 = @truncate(i);
+            bits[i] = if ((byte & (mask >> @as(U3, u8i))) != 0) @as(U1, 1) else @as(U1, 0);
+        }
+        try list.append(&bits);
+    }
+    try stdout.writeAll(&list);
+    try stdout.writeByte('\n');
+    defer list.deinit(); // Free memory when done
+}
